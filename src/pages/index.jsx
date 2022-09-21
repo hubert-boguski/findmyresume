48ec9ce45f1de1698dd53d87cfba8c40064d897e
hubert-boguski <108164994+hubert-boguski@users.noreply.github.com>
2022-09-21 13:19:17 -0400

    text out with meta data about website

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,28 @@
+import Head from "next/head";
+
+function Page({ children, title, description }) {
+  return (
+    <>
+      <Head>
+        <title> {title} </title>
+        <meta name = "description" content = {description}/>
+      </Head>
+      <div classname ="container mx-auto px-4">{children}</div>
+    </>
+  );
+}
+
 export default function Homepage() {
   return (
-    <div className="mt-16 container mx-auto">
-      <h1>Hello world.</h1>
-    </div>
+    <Page
+      {...{
+        title: "Find My Resume Website",
+        description: "Intereactive UX Portfolio Website Hubert Boguski",
+      }}
+    >
+      <div className = "font-medium">
+      Find My <span className = "text-red-500 font-medium"> Resume </span>
+      </div>
+    </Page>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,28 @@ module.exports = {
   ],
   important: true,
   theme: {
+    container: {
+      padding: '2rem',
+    },
     extend: {
-      colors: {},
+      fontFamily: {
+        medium: ["AirbnbCereal_W_Md"],
+      },
+      colors: {
+        gray: {
+          DEFAULT: "#161616",
+          50: "#FBFBFB",
+          100: "#E2E2E2",
+          200: "#AFAFAF",
+          300: "#7C7C7C",
+          400: "#494949",
+          500: "#161616",
+          600: "#0C0C0C",
+          700: "#020202",
+          800: "#000000",
+          900: "#000000",
+        },
+      },
     },
   },
   variants: {
