84d0df71f967000162d8b3af5355b0fac0b3c0ca
Hubert Boguski <108164994+hubert-boguski@users.noreply.github.com>
2022-09-30 20:21:07 -0400

    added colorful text on resume and funny subheader

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,8 +21,13 @@ export default function Homepage() {
       }}
     >
       <div className="flex items-center min-h-screen justify-center">
-        <div className = "mt-6 max-w-md space-y-8 text-center font-medium text-5xl">
-          Find My&nbsp;<span className="text-red-500">Resume</span>
+        <div className="mt-6 max-w-md space-y-2 text-center font-medium text-5xl">
+          <div className = "subpixel-antialiased">
+            Find My&nbsp;<span className="font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">Resume</span>
+          </div>
+          <div className="text-base font-mono">
+            Or click <span className="hover:underline text-gray-200 hover:text-gray-300 active:text-gray-400">here</span> if you're no fun :/
+          </div>
         </div>
       </div>
     </Page>
